5965e4ebbb3db39156a690d3e4974b180f943d7c
MarcosVxn <marcosjoaomj76@gmail.com>
2026-08-04 22:51:42 -0300

    Primeira Aula

---
+++ React/Aula01/painel-equipaments/src/components/EquipamentoCard.jsx
@@ -0,0 +1,12 @@
+const EquipamentoCard = ({ Card }) => {
+    const corStatus = {
+        "Em Uso": "yellow",
+        "Não tem": "red",
+        "Livre": "green"
+    }
+
+    return (
+        <div style ={{ border: "1px solid #1B263B", padding: "16px", bordera }}
+    );
+};
+
---
+++ React/Aula01/painel-equipaments/src/data/ordensEquipamento.js
@@ -0,0 +1,28 @@
+export const OrdemdeEquipamento = [
+    {
+        id: 1,
+        equipamento: "Sargento",
+        status: "Em Uso",
+        funcao: "Suporte de Peça"
+    },
+
+    {
+        id: 2,
+        equipamento: "Punção",
+        status: "Livre",
+        funcao: "Marcar furação"
+    },
+
+    {
+        id: 3,
+        equipamento: "Furadeira",
+        status: "Não tem",
+        funcao: "Fazer Furação"
+    },
+    {
+        id: 4,
+        equipamento: "Maçarico",
+        status: "Em Uso",
+        funcao: "Cortar aço"
+    }
+]
